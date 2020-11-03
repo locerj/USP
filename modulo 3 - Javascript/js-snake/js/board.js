ad781a23b4078082c00244e3a5320473472be89f
Marcelo Souza <locee_rj@hotmail.com>
2020-11-03 16:39:45 -0300

    spawn

--- modulo 3 - Javascript/js-snake/js/board.js
+++ modulo 3 - Javascript/js-snake/js/board.js
@@ -1,5 +1,7 @@
-let createBoard = (size = 20) => {
-    let board = {};
+let createBoard = (document, size = 20) => {
+    let board = {
+        size: size,
+    };
     board.render = () => {
         let rendered = "";
         for (let x = 0; x < size; x++) {
@@ -8,7 +10,14 @@ let createBoard = (size = 20) => {
                 rendered += `<td id="${y}:${x}"></td>`;
             }
         }
-        return rendered;
+        document.getElementById("board").innerHTML = rendered;
+    };
+
+    board.renderSnake = (snake) => {
+        let headSquare = document.getElementById(`${snake.position.y}:${snake.position.x}`
+        );
+
+        headSquare.className = "snakeHead";
     };
     return board;
-}
+};
